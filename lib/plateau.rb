2a037465c865ca7d6e97f5f972706a0eb1660205
Manoel do Nascimento <manu3105@yahoo.com>
2019-04-07 10:57:26 -0300

    Defined methods + solution

--- lib/plateau.rb
+++ lib/plateau.rb
@@ -1,9 +1,9 @@
 class Plateau
-  attr_reader :max_x, :max_y
+  attr_reader :max_width, :max_height
 
-  def initialize (max_x, max_y)
-    @max_x = max_x
-    @max_y = max_y
+  def initialize (max_width = 5, max_height = 5)
+    @max_width = max_width
+    @max_height = max_height
   end
   
 end
--- lib/rover.rb
+++ lib/rover.rb
@@ -1,21 +1,45 @@
 require_relative "plateau"
 
 class Rover
-  attr_accessor :coordinate_x, :coordinate_y, :orientation_letter
+  attr_reader :plateau
+  attr_accessor :current_width, :current_height, :orientation_letter
 
-  def initialize (coordinate_x, coordinate_y, orientation_letter)
-    @coordinate_x = coordinate_x
-    @coordinate_y = coordinate_y
+  def initialize (current_width = 0, current_height = 0, orientation_letter = "N", plateau)
+    @current_width = current_width
+    @current_height = current_height
     @orientation_letter = orientation_letter
+    @plateau = plateau
   end
 
   def turn_right
+    case @orientation_letter
+    when 'N' then @orientation_letter = 'E'
+    when 'S' then @orientation_letter = 'W'
+    when 'E' then @orientation_letter = 'S'
+    when 'W' then @orientation_letter = 'N'
+    end
   end
 
   def turn_left
-  end
-
-  def move
+    case @orientation_letter
+      when 'N' then @orientation_letter = 'W'
+      when 'S' then @orientation_letter = 'E'
+      when 'E' then @orientation_letter = 'N'
+      when 'W' then @orientation_letter = 'S'
+    end
   end
   
+  def move
+    case @orientation_letter
+      when 'N' then @current_height += 1 if @current_height < plateau.max_height
+      when 'S' then @current_height -= 1 if @current_height > 0
+      when 'E' then @current_width += 1 if @current_width < plateau.max_width
+      when 'W' then @current_width -= 1 if @current_width > 0
+    end
+  end
+
 end
+
+
+
+
